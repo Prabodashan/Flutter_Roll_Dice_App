81ad229c679e91564fe0855d870dea022e8120e3
Prabodashan <prabodashan14@gmail.com>
2025-01-20 22:57:59 +0530

    Add dice images and a button that, when clicked, randomly selects and displays a dice number.

---
+++ lib/gradient_container.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import 'package:roll_dice_app/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.colors1, this.colors2, {super.key});
+
+  final Color colors1;
+  final Color colors2;
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [colors1, colors2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(child: DiceRoller()),
+    );
+  }
+}
+
+
+//
+//class GradientContainer extends StatelessWidget {
+//   const GradientContainer({super.key, required this.colors});
+
+//   final List<Color> colors;
+
+//   @override
+//   Widget build(context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         gradient: LinearGradient(
+//           colors: colors,
+//           begin: startAlignment,
+//           end: endAlignment,
+//         ),
+//       ),
+//       child: const Center(child: StyledText('Hello World!')),
+//     );
+//   }
+// }
